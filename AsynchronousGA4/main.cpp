be795ce959bb4a04273aa198d98ddd5a580b025c
Bill Sellers <wis@mac.com>
2025-12-06 17:01:27 +0000

    Fix indent

--- AsynchronousGA4/main.cpp
+++ AsynchronousGA4/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setStyle(QStyleFactory::create("Fusion"));
-     MainWindow w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
