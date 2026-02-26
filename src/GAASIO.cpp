724ce2b1c79a115ccb4480f5dd8e9e21b98066a5
Bill Sellers <wis@mac.com>
2026-02-26 14:30:45 +0000

    Fixed WSL compile

--- src/GAASIO.cpp
+++ src/GAASIO.cpp
@@ -23,6 +23,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -131,7 +132,7 @@ int GAMain::Process(const std::string &parameterFile, const std::string &outputD
     auto currentTime = std::chrono::system_clock::now();
     auto localSecondsTime = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::current_zone()->to_local(currentTime)); //needs to be cast to seconds otherwise %S has decimal digits
     std::string timeString = std::format("{:%Y-%m-%d_%H.%M.%S}", localSecondsTime);
- #else
+#else
     time_t now = time(nullptr);
     struct tm local;
 #ifdef _MSC_VER
@@ -139,8 +140,8 @@ int GAMain::Process(const std::string &parameterFile, const std::string &outputD
 #else
     localtime_r(&now, &local);
 #endif
-    std::string timeString = ToString("%04d-%02d-%02d_%02d.%02d.%02d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday, local->tm_hour, local->tm_min, local->tm_sec);
- #endif
+    std::string timeString = ToString("%04d-%02d-%02d_%02d.%02d.%02d", local.tm_year + 1900, local.tm_mon + 1, local.tm_mday, local.tm_hour, local.tm_min, local.tm_sec);
+#endif
 
     if (m_baseXMLFile.GetSize() == 0)
     {
