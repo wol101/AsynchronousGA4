5944b60d553cd8342438e115a10872bb991df4d9
Bill Sellers <wis@mac.com>
2026-06-13 08:42:27 +0100

    Improving the disabled contrast on linux

--- AsynchronousGA4/main.cpp
+++ AsynchronousGA4/main.cpp
@@ -1,12 +1,26 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QStyle>
 #include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setStyle(QStyleFactory::create("Fusion"));
+
+#if defined(Q_OS_LINUX)
+    // Fix Fusion disabled-text contrast on Linux
+    if (QApplication::style()->objectName() == "fusion")
+    {
+        QPalette pal = a.palette();
+        pal.setColor(QPalette::Disabled, QPalette::Text, QColor("#A0A0A0"));
+        pal.setColor(QPalette::Disabled, QPalette::ButtonText, QColor("#A0A0A0"));
+        pal.setColor(QPalette::Disabled, QPalette::WindowText, QColor("#A0A0A0"));
+        a.setPalette(pal);
+    }
+#endif
+
     MainWindow w;
     w.show();
     return a.exec();
